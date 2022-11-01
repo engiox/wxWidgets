f598ad2a07b72fdb93d82050b95ffbccfa16eb0d
Me <cryoon@deviceeng.co.kr>
2022-11-01 15:05:45 +0900

    wip

--- zetcode/3_button/myFrame.cpp
+++ zetcode/3_button/myFrame.cpp
@@ -4,5 +4,6 @@ MyFrame::MyFrame(const wxString& title)
     : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(640, 480))
 {
     SetIcon(wxIcon("D:/dev/wxWidgets/art/wxwin.ico", wxBITMAP_TYPE_ICO, -1, -1));
+//    SetIcon(wxIcon(wxT("D:/dev/wxWidgets/art/wxwin32x32.xpm")));
     Centre();
 }
