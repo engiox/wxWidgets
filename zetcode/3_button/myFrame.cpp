f4e554ea44790331c6ad9520912831e21b3a0060
Crayoon <engio@naver.com>
2022-11-01 21:49:36 +0900

    WIP

--- zetcode/3_button/myFrame.cpp
+++ zetcode/3_button/myFrame.cpp
@@ -3,8 +3,8 @@
 MyFrame::MyFrame(const wxString& title)
     : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(640, 480))
 {
-//    SetIcon(wxIcon("D:/dev/wxWidgets/art/wxwin.ico", wxBITMAP_TYPE_ICO, -1, -1));
-    SetIcon(wxIcon(wxT("D:/dev/wxWidgets/art/wxwin32x32.xpm")));
+    SetIcon(wxIcon("D:/dev/wxWidgets/art/wxwin.ico", wxBITMAP_TYPE_ICO, -1, -1));
+//    SetIcon(wxIcon(wxT("D:/dev/wxWidgets/art/wxwin32x32.xpm")));
 
     wxPanel *panel = new wxPanel(this, wxID_ANY, wxPoint(20, 20), wxSize(200,100));
     wxButton *button = new wxButton(panel, wxID_EXIT, wxT("Quit"), wxPoint(20, 20));
